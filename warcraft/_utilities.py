e8052f5f87441f11a8ab3e82850ae272a233bd41
Markus Meskanen <markusmeskanen@gmail.com>
2016-07-21 13:12:37 +0300

    Added _utilities module with ClassProperty class
    The functions and classes in this module are meant to be used internally
    by the plugin.

---
+++ warcraft/_utilities.py
@@ -0,0 +1,74 @@
+"""Utility functions/classes needed internally by the plugin."""
+
+__all__ = (
+    'ClassProperty',
+)
+
+
+class ClassProperty:
+    """Read-only property for classes instead of instances.
+
+    Acts as a combination of :func:`property` and :func:`classmethod`
+    to create properties for classes.
+    The recommended usage of :class:`ClassProperty` is as a decorator:
+
+    .. code-block:: python
+
+        class My_Class:
+
+            @ClassProperty
+            def name(cls):
+                return cls.__name__.replace('_', ' ')
+
+        obj = My_Class()
+
+        print('Accessed through the class:', My_Class.name)
+        print('Accessed through the object:', obj.name)
+
+        class My_Subclass_One(My_Class):
+            pass
+
+        class My_Subclass_Two(My_Class):
+            # Override the classproperty
+            name = 'My_Subclass: 2'
+
+        print('Accessed through subclass 1:', My_Subclass_One.name)
+        print('Accessed through subclass 2:', My_Subclass_Two.name)
+
+    Output:
+
+    .. code-block:: none
+
+        Accessed through the class: My Class
+        Accessed through the object: My Class
+        Accessed through subclass 1: My Subclass One
+        Accessed through subclass 2: My_Subclass: 2
+    """
+
+    def __init__(self, fget=None, doc=None):
+        """Initialize the class property with a get function.
+
+        :param callable|None fget:
+            Function to call when the property is read
+        :param str|None doc:
+            Docstring, automatically copied from ``fget`` if None
+        """
+        if doc is None and fget is not None:
+            doc = fget.__doc__
+        self.fget = fget
+        self.__doc__ = doc
+
+    def __get__(self, obj=None, type_=None):
+        """Call :attr:`fget` when the class property is read.
+
+        :param object obj:
+            Object accessing the class property (can be None)
+        :param type type_:
+            Class accessing the class property
+
+        If ``type_`` is ``None`` but an object was provided, ``type_``
+        will be recieved from ``type(obj)``.
+        """
+        if type_ is None and obj is not None:
+            type_ = type(obj)
+        return self.fget(type_)
