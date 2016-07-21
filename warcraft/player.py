78b2bc617a38038abe02b5c0a439972f3ca58351
Markus Meskanen <markusmeskanen@gmail.com>
2016-07-21 14:23:30 +0300

    Added player module with Player class

---
+++ warcraft/player.py
@@ -0,0 +1,46 @@
+# Python 3 imports
+import collections
+
+# Custom Source.Python imports
+import easyplayer
+
+# Warcraft imports
+from warcraft.entities import Hero
+
+__all__ = (
+    'Player',
+)
+
+
+class Player(easyplayer.Player):
+    """Player class with support for managing Warcraft heroes.
+
+    Each player has an ordered dictionary of his heroes in the format
+    of ``{hero.class_id: hero}`` stored into :attr:`heroes` attribute.
+    The player also has a :attr:`hero` attribute to store the hero
+    he's currently playing with.
+    """
+
+    def __init__(self, index):
+        """Initialize the player.
+
+        :param int index:
+            Index of the player entity
+        """
+        super().__init__(index)
+        self.heroes = collections.OrderedDict()
+        self._hero = None
+
+    @property
+    def hero(self):
+        return self._hero
+
+    @hero.setter
+    def hero(self, value):
+        if not isinstance(value, Hero):
+            raise ValueError(
+                "Attempt to set player's hero to a non hero value {0}".format(value))
+        if value.class_id not in self.heroes:
+            raise ValueError(
+                "Hero {0} not owned by player.".format(value))
+        self._hero = value
