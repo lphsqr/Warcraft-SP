6606eb39fd4fd5a5c382801548a9f6fbf5d73b8c
Markus Meskanen <markusmeskanen@gmail.com>
2016-08-17 09:04:15 +0300

    Removed all database functionality

--- srcds/addons/source-python/plugins/warcraft/warcraft.py
+++ srcds/addons/source-python/plugins/warcraft/warcraft.py
@@ -1,22 +1,16 @@
 """Main entry point for the plugin."""
 
-# Python 3 imports
-import contextlib
-
 # Source.Python imports
 from commands import CommandReturn
 from commands.client import ClientCommand
 from commands.say import SayCommand
 from events import Event
-from listeners.tick import TickRepeat
 from menus import ListMenu
 from menus import ListOption
 from menus import PagedMenu
 from menus import PagedOption
 from messages import SayText2
-from paths import PLUGIN_DATA_PATH
 from players.dictionary import PlayerDictionary
-from players.helpers import index_from_userid
 from translations.strings import LangStrings
 
 # Warcraft imports
@@ -26,21 +20,12 @@ import warcraft.player
 
 
 # ======================================================================
-# >> DATABASE MANAGEMENT
+# >> FUNCTIONS
 # ======================================================================
 
 def _new_player(index):
     """Create a player and load his data from the database."""
     player = warcraft.player.Player(index)
-    steamid = player.steamid
-
-    # Load heroes
-    for hero_id, level, xp in database.get_heroes_data(steamid):
-        with contextlib.suppress(KeyError):
-            hero = player.heroes[hero_id] = heroes[hero_id](player, level, xp)
-            # And their skills
-            for skill_id, level in database.get_skills_data(steamid, hero_id):
-                hero.skills[skill_id].level = level
 
     # Give the player all heroes available by his total level
     total_level = player.calculate_total_level()
@@ -51,71 +36,9 @@ def _new_player(index):
             player.heroes[hero_id] = heroes[hero_id](player)
 
     # Set player's active hero
-    active_hero_id = database.get_active_hero_id(steamid)
-    if active_hero_id is not None:
-        player.hero = player.heroes[active_hero_id]
-    else:
-        player.hero = next(iter(player.heroes.values()))
+    player.hero = next(iter(player.heroes.values()))
 
     return player
-
-
-def _serialize_player_data(player):
-    """Serialize player's data for other functions to save it."""
-    steamid = player.steamid
-    hero = player.hero
-    return (
-        # players
-        (steamid, hero.class_id),
-        # heroes
-        (steamid, hero.class_id, hero.level, hero.xp),
-        # skills
-        (
-            (steamid, hero.class_id, skill_id, skill.level)
-            for skill_id, skill in hero.skills.items()
-        ),
-    )
-
-def _save_player_data(player, *,  commit=True):
-    """Save individual player's data into the database."""
-    player_data, hero_data, skills_data = _serialize_player_data(player)
-    database.save_player(player_data)
-    database.save_hero(hero_data)
-    database.save_skills(skills_data)
-    if commit:
-        database.commit()
-
-
-def _save_all_data(*, commit=True):
-    """Save every active player's data into the database."""
-    datas = (_serialize_player_data(player) for player in players.values())
-    try:
-        players, heroes, skills_list = zip(*datas)
-    except ValueError:
-        return
-    skills = [skill for skills in skills_list for skill in skills]  # Flatten
-    database.save_players(players)
-    database.save_heroes(heroes)
-    database.save_skills(skills)
-    if commit:
-        database.commit()
-
-
-def unload():
-    """Store players' data and close the database."""
-    _data_save_repeat.stop()
-    _save_all_data()
-    database.close()
-
-
-@Event('player_disconnect')
-def _save_disconnecters_data(event):
-    """Save player's data upon disconnect."""
-    index = index_from_userid(event['userid'])
-    if index not in players:
-        return
-    _save_player_data(players[index])
-    del players[index]
 
 
 # ======================================================================
@@ -232,13 +155,6 @@ players = PlayerDictionary(_new_player)
 
 # A dictionary of the heroes from heroes.__init__.get_heroes
 heroes = {hero.class_id: hero for hero in warcraft.heroes.get_heroes()}
-
-# Database wrapper for accessing the Warcraft database
-database = warcraft.database.SQLite(PLUGIN_DATA_PATH / 'warcraft.db')
-
-# A tick repeat for saving everyone's data every 4 minutes
-_data_save_repeat = TickRepeat(_save_all_data)
-_data_save_repeat.start(240, 0)
 
 # Translations for the Warcraft plugin
 _tr = LangStrings('warcraft')
